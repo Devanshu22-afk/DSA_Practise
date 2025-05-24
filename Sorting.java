d6c2a06d834f86d2bf783446908f54559df59833
Devanshu Bhonde <devanshu.bhonde@gmail.com>
2025-05-25 00:40:27 +0530

    Add Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,28 @@
+//Bubble Sort
+
+// import java.util.Arrays;
+
+// public class Sorting {
+//    public static void main(String[]args){
+
+//       int arr[]={1,7,3,4,8,12,-2,6};
+//       bubblesort(arr);
+//       System.err.println(Arrays.toString(arr));
+//    }
+//    public static void swap(int[] arr, int i, int j) {
+//     int temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+// }
+
+//    public static void bubblesort(int arr[]){
+//       for(int i=0;i<arr.length;i++){
+//          for(int j=1;j<arr.length-i;j++){
+//             if(arr[j]<arr[j-1]){
+//                swap(arr,j,j-1);
+//             }
+//          }
+//       }
+
+//    }
+// }
