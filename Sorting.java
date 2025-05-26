23c8d1fc20ef6780c36f9746b9cd69afa511b876
Devanshu Bhonde <devanshu.bhonde@gmail.com>
2025-05-26 21:21:46 +0530

    Selection Sort

--- Sorting.java
+++ Sorting.java
@@ -26,3 +26,39 @@
 
 //    }
 // }
+
+import java.util.Arrays;
+
+public class Sorting{
+
+   public static void main(String[] args) {
+      int arr[]={1,4,5,6,7,8,15,34};
+      selectionsort(arr);;
+      System.out.println(Arrays.toString(arr));
+
+   }
+
+   static void swap(int[]arr,int a ,int b){
+      int temp=arr[b];
+      arr[b]=arr[a];
+      arr[a]=temp;
+   }
+
+   static void selectionsort(int[]arr){
+      for(int i=0;i<arr.length;i++){
+         int last=arr.length-i-1;
+         int max=Largest(arr,0,last);
+         swap(arr, max, last);
+      }
+   }
+   static int Largest(int[] arr,int start,int end){
+      int max=start;
+      for(int i=start;i<=end;i++){   
+         if(arr[i]>arr[max]){
+            max=i;
+         }
+      }
+      return max;
+
+   }
+}
